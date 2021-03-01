d62753fac1790dd0c84257ddea4c60e3ed6f4cbe
valkam08 <51866926+valkam08@users.noreply.github.com>
2021-03-01 21:18:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<p align="center"><img src="./app/assets/images/Logo.png" width="150px" height="150px" alt="aventium softworks"></p>
+
+<h1 align="center">Nercoria Launcher</h1>
+
+<p align="center">Fork of <a href="https://github.com/dscalzi/HeliosLauncher/">Helios Launcher</a></p>
+
---
+++ build.js
@@ -0,0 +1,68 @@
+const builder = require('electron-builder')
+const Platform = builder.Platform
+
+function getCurrentPlatform(){
+    switch(process.platform){
+        case 'win32':
+            return Platform.WINDOWS
+        case 'darwin':
+            return Platform.MAC
+        case 'linux':
+            return Platform.linux
+        default:
+            console.error('Cannot resolve current platform!')
+            return undefined
+    }
+}
+
+builder.build({
+    targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
+    config: {
+        appId: 'nercorialauncher',
+        productName: 'Nercoria Launcher',
+        artifactName: '${productName}-setup-${version}.${ext}',
+        copyright: 'Copyright © 2021 Nercoria Development Team',
+        directories: {
+            buildResources: 'build',
+            output: 'dist'
+        },
+        win: {
+            target: [
+                {
+                    target: 'nsis',
+                    arch: 'x64'
+                }
+            ]
+        },
+        nsis: {
+            oneClick: false,
+            perMachine: false,
+            allowElevation: true,
+            allowToChangeInstallationDirectory: true
+        },
+        mac: {
+            target: 'dmg',
+            category: 'public.app-category.games'
+        },
+        linux: {
+            target: 'AppImage',
+            maintainer: 'Valkam',
+            vendor: 'Nercoria',
+            synopsis: 'Launcher de Nercoria',
+            description: 'Envie de jouer à Nercoria ? Alors rejoignez-nous dès maintenant.',
+            category: 'Game'
+        },
+        compression: 'maximum',
+        files: [
+            '!{dist,.gitignore,.vscode,docs,dev-app-update.yml,.travis.yml,.nvmrc,.eslintrc.json,build.js}'
+        ],
+        extraResources: [
+            'libraries'
+        ],
+        asar: true
+    }
+}).then(() => {
+    console.log('Build complete!')
+}).catch(err => {
+    console.error('Error during build!', err)
+})
